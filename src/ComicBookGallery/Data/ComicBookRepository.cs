a8697f3c445e2a2387750f6cb275856bea5859a2
Ted Dunn <tldunn451@gmail.com>
2017-03-07 09:51:28 -0500

    Add Comic Books List view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -61,6 +61,11 @@ namespace ComicBookGallery.Data
             }
         };
 
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
